6dadb48655dfd3cc58f3db1aa4f2c23ddc2c97c7
Khinpanpanpan <kawkaw2000232@gmail.com>
2026-06-12 09:06:54 +0630

    mvc

---
+++ BookReadingTracker.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookReadingTracker.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
